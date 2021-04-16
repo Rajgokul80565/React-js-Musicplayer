90dad3278e786779260d8c25b3defffc19664a76
Rajgokul80565 <74341639+Rajgokul80565@users.noreply.github.com>
2021-04-16 21:05:50 +0530

    changed different song

--- src/components/songsList.js
+++ src/components/songsList.js
@@ -1,48 +1,29 @@
 
 import bunny from '../images/bunny.jpg';
 import spidey from '../images/spidey.jpeg';
-import music1 from '../music/Enjoy Enjaami.mp3';
-import music2 from '../music/godzilla.mp3'
-import  music3 from '../music/Way_Out.mp3';
+import music1 from '../music/Cobie Sample.mp3';
+import music2 from '../music/jazz study.mp3'
+import  music3 from '../music/True Friends.mp3';
 
   const songsList = [
     { 
-        title: "Way Out",
-        artist:"Jack Harlow ft Big sean",
+        title: "Cobie Sample",
+        artist:"Lofi beat by Afromac",
         img_src: spidey,
-        src: music3,
+        src: music1,
   
     },
     { 
-        title: "Godzilla",
-        artist:"Eminem and Juice wrld",
+        title: "Jazz Study",
+        artist:"Lofi beat by chillcow",
         img_src:bunny,
         src: music2 ,
     },
     {
-        title: "Enjoy Enjami",
-        artist:"Dhee and Arivu",
-  
+        title: "True Friends",
+        artist:"ChillCow",
         img_src:bunny,
-        src: music1 ,
-    },
-    {
-      title: "Godzilla",
-      artist:"Eminem and Juice wrld",
-      img_src:bunny,
-      src: music2 ,
-    },
-    {
-      title: "Godzilla",
-      artist:"Eminem and Juice wrld",
-      img_src:bunny,
-      src: music2 ,
-    },
-    {
-      title: "Godzilla",
-      artist:"Eminem and Juice wrld",
-      img_src:bunny,
-      src: music2 ,
+        src: music3 ,
     }
 
 ];
